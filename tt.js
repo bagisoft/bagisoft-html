82311ad0bc890095b07e03f7ca978306c58fce78
bagisoft <37068873+bagisoft@users.noreply.github.com>
2018-03-17 01:44:43 +0700

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,40 @@
+<script type="text/javascript">
+  var message="Function right click is Disabled!";
+  function clickIE4(){
+    if(event.button==2){
+      alert(message);
+      return false;
+    }
+  }
+  function clickNS4(e){
+    if(document.layers||document.getElementById&&!document.all){
+      if(e.which==2||e.which==3){
+        alert(message);
+        return false;
+      }
+    }
+  }
+  if(document.layers){
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown=clickNS4;
+  }else if(document.all&&!document.getElementById){
+    document.onmousedown=clickIE4;
+  }
+  document.oncontextmenu=new Function("alert(message);return false");
+</script>
+<script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "9e792050f1dcde8a041ea70e7d4de3b1",
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>
